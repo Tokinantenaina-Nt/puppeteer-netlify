f102983041b7af7356f8829fd2b38943a44c7247
Tokinantenaina <tokib2oba@yahoo.com>
2024-01-15 05:15:07 +0300

    puppeteer-netlify

---
+++ launcher.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer-core');
+const path = require('path');
+const random = Math.floor(Math.random() * 100);
+async function capture() {
+    const browser = await puppeteer.launch({
+        executablePath: path.join(__dirname, 'Slimjet', 'slimjet.exe'),
+        headless: true
+    });
+    const page = await browser.newPage();
+
+    //await page.goto('https://www.getintopc.com');
+    await page.goto('https://www.flashscore.mobi/?s=2');
+
+    // await new Promise(resolve => setTimeout(resolve, 2000)); // Pause de 5 secondes
+    await page.screenshot({ path: `screenshot-${random}.png` });
+
+    await browser.close();
+}
+
+module.exports = capture;
