999cea7557ee77f9dc575db8506bdfe5623f3333
Tokinantenaina-Nt <tokib2oba@yahoo.com>
2024-01-18 10:38:59 +0300

    corr get pupna,e

--- routes.js
+++ routes.js
@@ -12,53 +12,48 @@ router.get('/', (req, res) => {
 });
 
 router.get('/pup/:name', async (req, res) => {
-    const name = req.params.name
+    const name = req.params.name;
     try {
-        const browser = await puppeteer.launch(
-            //   { executablePath: 'E:\\Slimjet\\slimjet.exe' }
-            {
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: await chromium.executablePath(),
-                headless: chromium.headless,
-                ignoreHTTPSErrors: true,
-            }
-        );
+        const browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+        });
         const page = await browser.newPage();
         await page.goto('https://www.flashscore.mobi/?s=2');
 
         const screenshot = await page.screenshot();
 
         await browser.close();
-        await b2.uploadFile(screenshot, {
-            name: `screenshot-${name}.png`, // Nom du fichier sur Backblaze B2
+        b2.uploadFile(screenshot, {
+            name: `screenshot-${name}.png`,
             bucket: 'screenshot-netlify',
-        },
-            function (err, response) {
-                if (err) {
-                    console.error('Une erreur s\'est produite :', err);
-                    res.status(500).send('Erreur interne du serveur ###', err);
-                } else {
-                    res.status(200).json({ message: 'Done!', response });
-                }
+        }, (err, response) => {
+            if (err) {
+                console.error('Une erreur s\'est produite lors de l\'upload sur Backblaze B2 :', err);
+                res.status(500).send('Erreur interne du serveur : ' + err.message);
+            } else {
+                console.log('Upload réussi sur Backblaze B2 :', response);
+                res.status(200).json({ message: 'Capture d\'écran uploadée avec succès!', response });
             }
-        );
-
-
+        });
     } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
-        res.status(500).send('Erreur interne du serveur ::: ', error);
+        console.error('Une erreur s\'est produite lors de la capture d\'écran :', error);
+        res.status(500).send('Erreur interne du serveur : ' + error.message);
     }
 });
 
-router.get('/getImageURL', (req, res) => {
 
+router.get('/getImageURL/:name', (req, res) => {
+    const name = req.params.name
     try {
-        const imageUrl = 'https://f004.backblazeb2.com/file/screenshot-netlify/screenshot-1.png';
+        const imageUrl = `https://f004.backblazeb2.com/file/screenshot-netlify/screenshot-${name}.png`;
         res.status(200).json({ imageUrl });
     } catch (error) {
         console.error('Une erreur s\'est produite lors de la récupération de l\'URL de l\'image :', error);
-        res.status(500).json({ error: 'Erreur interne du serveur' });
+        res.status(500).json({ error: 'Erreur interne du serveur ###', error });
     }
 
 });
